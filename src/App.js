ac20e16eed1208b21729e2b53b2919c5d0d05088
Ramyarnayak <ramya.nayak@ymedialabs.com>
2020-12-07 10:27:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+// App.propTypes = {
+//   children: PropTypes.object.isRequired
+// };
+
+export default App;
